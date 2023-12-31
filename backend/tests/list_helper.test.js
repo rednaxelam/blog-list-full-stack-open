9300c051242d5191a02939bccd1278f6ef2441cd
Michael <michael.alexander0008@gmail.com>
2023-12-31 13:03:48 +0000

    Complete Exercise 4.4

---
+++ backend/tests/list_helper.test.js
@@ -0,0 +1,25 @@
+const listHelper = require('../utils/list_helper')
+const { listWithOneBlog, blogs } = require('./list_helper_constants')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+  test('Of empty blog list is 0', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+
+  test('Of blog list with only one blog post equals likes for that blog post', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+  })
+
+  test('Of a larger blog list is calculated correctly', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
+
+})
--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogList => {
+  return blogList.reduce(
+    (totalLikes, blogPost) => totalLikes + blogPost.likes,
+    0
+  )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
