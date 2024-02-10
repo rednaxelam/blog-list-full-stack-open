a746a5cf2ee8000720ece9467e698c738579699b
Michael <michael.alexander0008@gmail.com>
2024-02-10 01:07:07 +0000

    Complete Exercises 5.7 and 5.8

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetailed, setShowDetailed] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (showDetailed) {
+    return <div style={blogStyle}>
+      <p>{blog.title} {blog.author} <button onClick={() => setShowDetailed(false)}>hide</button></p>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes} <button>like</button></p>
+      <p>{blog.user.name}</p>
+    </div>
+  } else {
+    return <div style={blogStyle}>
+      <p>{blog.title} {blog.author} <button onClick={() => setShowDetailed(true)}>view</button></p>
+    </div>
+  }
+}
 
 export default Blog
