1f6b96ce25bb7e566b270ca9cfac2c390d897128
Michael <michael.alexander0008@gmail.com>
2024-02-10 14:32:23 +0000

    Complete Exercise 5.10

--- frontend/src/components/BlogList.jsx
+++ frontend/src/components/BlogList.jsx
@@ -3,7 +3,7 @@ import Blog from "./Blog"
 const BlogList = ({ blogs, setBlogs, setOutcomeMessage }) => {
   return (
     <div>
-      { blogs.map(blog => <Blog
+      { blogs.toSorted((blog1, blog2) => blog2.likes - blog1.likes).map(blog => <Blog
           key={blog.id}
           blog={blog}
           setBlogs={setBlogs}
