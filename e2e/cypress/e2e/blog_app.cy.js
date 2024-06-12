eec6675a575cdcf7eb1e19cecfbda888c2b634a3
Michael <michael.alexander0008@gmail.com>
2024-06-12 16:11:23 +0100

    Complete Exercise 5.18

--- e2e/cypress/e2e/blog_app.cy.js
+++ e2e/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      username: 'mluukkai',
+      password: 'salainen',
+      name: 'Bob'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -7,5 +14,23 @@ describe('Blog app', function() {
     cy.get('#username')
     cy.get('#pwd')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#pwd').type('salainen')
+      cy.contains('login').click()
+
+      cy.contains('Bob logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#pwd').type('wrong')
+      cy.contains('login').click()
+
+      cy.contains('Login Unsuccessful').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
